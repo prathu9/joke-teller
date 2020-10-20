1f23bd8caae6574d0a83ed065b36469f83718b42
prathamesh sawant <prathameshpgs@gmail.com>
2020-10-20 23:39:29 +0530

    Added Jokes API and modified styling for image view

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 const button = document.getElementById("button");
 const roboContainer = document.querySelector(".robo-container");
+let speech = new SpeechSynthesisUtterance();
 
-
-const changeBG = ()=>{
-    let msg = new SpeechSynthesisUtterance();
-    msg.text = "Currently there are no jokes";
-    window.speechSynthesis.speak(msg);
-    console.log(speechSynthesis);
-    roboContainer.setAttribute("class", "robo-container start-joke");
-    setTimeout(()=>{roboContainer.setAttribute("class", "robo-container stop-joke");},2000)
+const tellJoke = (joke, jokeTime)=>{
+    try{
+        speech.text = joke;
+        window.speechSynthesis.speak(speech);
+        if(joke){
+            roboContainer.setAttribute("class", "robo-container start-joke");
+        }
+        setTimeout(()=>{roboContainer.setAttribute("class", "robo-container stop-joke");},jokeTime)
+    }
+    catch(error){
+        console.error(`Error in tellJoke: ${error}`);
+    }
 }
 
-button.addEventListener("click", changeBG);
+//Get Jokes from Joke API
+const getJokes = async ()=>{
+    try{
+        let joke = "";
+        const jokeApiUrl = `https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist`;
+        const response = await fetch(jokeApiUrl);
+        const data = await response.json();
+        if(data.setup){
+            joke = `${data.setup} ${data.delivery}`;
+        }else{
+            joke = data.jokeTime;
+        }
+        tellJoke(joke, Math.floor(joke.length/15)*1000);
+    }
+    catch(error){
+        tellJoke("Sorry I have no jokes, try again");
+    }
+    
+}
+
+// 
+button.addEventListener("click", getJokes);
